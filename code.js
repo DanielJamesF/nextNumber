beccf4aa08d56e4380c5d5c5bc9edba1911125fa
DanielJamesF <fredericksd17@gmail.com>
2022-06-17 16:05:50 +0200

    first commit

---
+++ code.js
@@ -0,0 +1,16 @@
+let numArray = [1,2,3,4,5,6];
+
+const iteration = numArray.values();
+let one = iteration.next().value;
+console.log(one)
+let two = iteration.next().value;
+console.log(two)
+let three = iteration.next().value;
+console.log(three)
+let four = iteration.next().value;
+console.log(four)
+let five = iteration.next().value;
+console.log(five)
+let six = iteration.next().value;
+console.log(six)
+let seven = iteration.next().value;
